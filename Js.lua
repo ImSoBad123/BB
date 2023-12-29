20a0c86c1643c4b1b0aa75500445122e75179660
ImSoBad123 <138328968+ImSoBad123@users.noreply.github.com>
2023-12-29 23:35:59 +0700

    Update Js.lua

--- Js.lua
+++ Js.lua
@@ -1,31 +1,93 @@
-            if game.Workspace.Map:FindFirstChild("Arena") then
-                game.Workspace.Map.Arena:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Quarry") then
-                game.Workspace.Map.Quarry:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("RingOfFire") then
-                game.Workspace.Map.RingOfFire:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Desert") then
-                game.Workspace.Map.Desert:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Classic_V2") then
-                game.Workspace.Map.Classic_V2:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Heaven") then
-                game.Workspace.Map.Heaven:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Height_Fight") then
-                game.Workspace.Map.Height_Fight:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("WinterCountryside") then
-                game.Workspace.Map.WinterCountryside:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Enchanted_Desert") then
-                game.Workspace.Map.Enchanted_Desert:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("PolarExpress") then
-                game.Workspace.Map.PolarExpress:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Grassy_Classic") then
-                game.Workspace.Map.Grassy_Classic:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Ocean") then
-                game.Workspace.Map.Ocean:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Underworld") then
-                game.Workspace.Map.Underworld:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            elseif game.Workspace.Map:FindFirstChild("Jungle") then
-                game.Workspace.Map.Jumgle:FindFirstChild("Sock").CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-            else
-            print("ko thay map")
-            end
+if game.Workspace.Map:FindFirstChild("Arena") then
+    for _, child in ipairs(game.Workspace.Map.Arena:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Quarry") then
+    for _, child in ipairs(game.Workspace.Map.Quarry:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("RingOfFire") then
+    for _, child in ipairs(game.Workspace.Map.RingOfFire:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Desert") then
+    for _, child in ipairs(game.Workspace.Map.Desert:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Classic_V2") then
+    for _, child in ipairs(game.Workspace.Map.Classic_V2:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Heaven") then
+    for _, child in ipairs(game.Workspace.Map.Heaven:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Height_Fight") then
+    for _, child in ipairs(game.Workspace.Map.Height_Fight:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("WinterCountryside") then
+    for _, child in ipairs(game.Workspace.Map.WinterCountryside:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Enchanted_Desert") then
+    for _, child in ipairs(game.Workspace.Map.Enchanted_Desert:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("PolarExpress") then
+    for _, child in ipairs(game.Workspace.Map.PolarExpress:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Grassy_Classic") then
+    for _, child in ipairs(game.Workspace.Map.Grassy_Classic:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Ocean") then
+    for _, child in ipairs(game.Workspace.Map.Ocean:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Underworld") then
+    for _, child in ipairs(game.Workspace.Map.Underworld:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Jungle") then
+    for _, child in ipairs(game.Workspace.Map.Jungle:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+elseif game.Workspace.Map:FindFirstChild("Classic") then
+    for _, child in ipairs(game.Workspace.Map.Classic:GetChildren()) do
+        if child.Name == "Sock" then
+            child.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+        end
+    end
+else
+print("ko thay map")
+end
